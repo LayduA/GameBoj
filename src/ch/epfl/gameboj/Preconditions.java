66ea9ccbd963f9df3162ecc0815c6858c22ba274
Adrien Laydu <adrien.laydu@epfl.ch>
2018-02-20 22:41:20 +0100

    encore oui

--- src/ch/epfl/gameboj/Preconditions.java
+++ src/ch/epfl/gameboj/Preconditions.java
@@ -1,5 +1,29 @@
-
+package gameboj;
 
 public interface Preconditions {
 
+    public static void checkArgument(boolean b) {
+        
+        if (!(b)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static int checkBits8(int v) {
+        
+        if (!(v >= 0 && v <= 0xFF)) {
+            throw new IllegalArgumentException();
+        } else {
+            return v;
+        }
+    }
+    
+    public static int checkBits16(int v) {
+        
+        if (!(v >= 0 && v <= 0xFFFF)) {
+            throw new IllegalArgumentException();
+        } else {
+            return v;
+        }
+    }
 }
