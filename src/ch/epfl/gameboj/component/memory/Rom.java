f1bc6e6d53e2e2c93a21e7a31ec6b5e0b7924bf0
Michaël-PC\Michaël <michael.tasev@gmail.com>
2018-02-21 00:15:45 +0100

    oui

--- src/ch/epfl/gameboj/component/memory/Rom.java
+++ src/ch/epfl/gameboj/component/memory/Rom.java
@@ -1,5 +1,29 @@
 package gameboj.component.memory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import gameboj.Preconditions;
 
-public class Rom {
+public final class Rom {
 
+    byte rom[];
+    
+    int l; 
+    
+    public Rom(byte[] data) {
+        l = data.length;
+        byte data2[] = Arrays.copyOf(data, l);
+        rom = data2;
+    }
+
+    public int size() {
+        return rom.length;
+    }
+    
+    public int read(int index) {
+        Preconditions.checkBits8(index);
+        
+        if (!(index >= 0 && index <= 0xFF)) {
+            throw new IndexOutOfBoundsException();
+        } else return rom.indexOf(index);
+    }
 }
