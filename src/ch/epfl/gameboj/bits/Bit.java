168f20c1524f5957c9c1ea14ac9c0f9a65ab9012
Adrien Laydu <adrien.laydu@epfl.ch>
2018-02-22 18:55:14 +0100

    jaj

---
+++ src/ch/epfl/gameboj/bits/Bit.java
@@ -0,0 +1,15 @@
+package gameboj.bits;
+
+public interface Bit {
+    
+    public abstract int ordinal();
+    
+    public default int index() {
+        return ordinal();
+    }
+    
+    public default int mask() {
+        //TODO : implement
+        return 0;
+    }
+}
