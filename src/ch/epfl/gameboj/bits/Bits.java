7f0eef3fd99714e597b35ead715aa1c0fea72d76
Adrien Laydu <adrien.laydu@epfl.ch>
2018-02-22 20:43:28 +0100

    MDR

--- src/ch/epfl/gameboj/bits/Bits.java
+++ src/ch/epfl/gameboj/bits/Bits.java
@@ -1,12 +1,13 @@
 package gameboj.bits;
 
-import java.util.Objects;
-
 public final class Bits {
     
     private Bits() {}
     
     public static int mask(int index) {
-        if(O)
+        if (index<0 || index >31) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (1 << index);
     }
 }
