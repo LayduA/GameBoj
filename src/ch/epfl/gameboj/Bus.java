4e47d1677a61d78420bcda94c4786acd1eaf6e28
Adrien Laydu <adrien.laydu@epfl.ch>
2018-02-22 18:41:41 +0100

    Bus sus

--- src/ch/epfl/gameboj/Bus.java
+++ src/ch/epfl/gameboj/Bus.java
@@ -18,7 +18,25 @@ public final class Bus {
     }
     
     public int read(int address) {
-        int value = 0xFF;
-        
+       int value=0xFF;
+       boolean found = false;
+       int i = 0;
+       do {
+           if ((components.get(i)).read(address) != Component.NO_DATA){
+               value = (components.get(i)).read(address);
+               found = true;
+           }
+           i++;
+       }while(!found&&i<components.size());
+       
+       return value;
+    }
+    
+    public void write(int address, int data) {
+        Preconditions.checkBits16(address);
+        Preconditions.checkBits8(data);
+        for (int i =0;i<components.size();i++) {
+            (components.get(i)).write(address, data);
+        }
     }
 }
