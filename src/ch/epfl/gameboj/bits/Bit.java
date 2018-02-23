08a55ce120797394590782c7a1dd60093e5b8920
Michaël-PC\Michaël <michael.tasev@gmail.com>
2018-02-23 13:10:43 +0100

    oui

--- src/ch/epfl/gameboj/bits/Bit.java
+++ src/ch/epfl/gameboj/bits/Bit.java
@@ -1,15 +1,17 @@
 package gameboj.bits;
 
 public interface Bit {
-    
+
     public abstract int ordinal();
-    
+
     public default int index() {
         return ordinal();
     }
-    
+
     public default int mask() {
-        //TODO : implement
+        // TODO : implement
         return 0;
+
     }
+
 }
