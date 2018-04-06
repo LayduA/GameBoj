ee6aa6b67e1b14bceb7adf75c5aa21e37bf0171a
Adrien Laydu <adrien.laydu@epfl.ch>
2018-04-06 16:16:00 +0200

    oui

--- src/ch/epfl/gameboj/DebugMain.java
+++ src/ch/epfl/gameboj/DebugMain.java
@@ -10,7 +10,7 @@ import ch.epfl.gameboj.component.cpu.Cpu;
 public final class DebugMain {
     
     public static void main(String[] args) throws IOException {
-        File romFile = new File("instr_timing.gb");
+        File romFile = new File(args[0]);
         long cycles = Long.parseLong(args[1]);
 
         GameBoy gb = new GameBoy(Cartridge.ofFile(romFile));
